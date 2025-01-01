414bae2681cc70603c7e02b77a710aa50e6d24ae
bryandebourbon <bryandebourbon@gmail.com>
2025-01-01 15:41:15 -0500

    Add/update file Tests/YogaPosesWidgetTests/DummyTests.swift

---
+++ Tests/YogaPosesWidgetTests/DummyTests.swift
@@ -0,0 +1,9 @@
+import XCTest
+@testable import YogaPosesWidget
+
+final class DummyTests: XCTestCase {
+    func testExample() throws {
+        // This is a dummy test so SwiftPM sees a Tests target.
+        XCTAssertTrue(true)
+    }
+}
